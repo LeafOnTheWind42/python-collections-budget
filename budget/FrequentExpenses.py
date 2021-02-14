9f2bc3e83a5bc6dc4d34c99b0af5336cf6c1f58f
crw440 <crw440@gmail.com>
2021-02-13 19:46:43 -0500

    Updated tests

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -6,8 +6,7 @@ from . import Expense
 
 
 expenses = Expense.Expenses()
-expenses.read_expenses(
-    r"C:\Users\user\PycharmProjects\python-collections-budget\data/spending_data.csv")
+expenses.read_expenses("data/spending_data.csv")
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
